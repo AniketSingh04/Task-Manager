9b2a3dc31c522bfaf40a42b4c7571362b2d7a3c6
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-05-17 16:03:36 +0530

    Main Server

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks.js");
+
+//middleware
+app.use(express.json()) //for handling json requests
+
+
+//routes
+app.get("/hello", (req, res) =>{
+    console.log("Hello");
+    res.send("Task Manager")
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 3000;
+app.listen(3000, () => {
+    console.log(`Server is listening on port ${3000}`);
+})
