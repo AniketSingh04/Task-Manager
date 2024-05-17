d7a7da57e8ae9d4a2e4c4793d37f885fb6f47908
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-05-18 00:12:27 +0530

    CRUD

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,10 +1,15 @@
+const Task = require("../models/Task.js")
+
+
 const getAllTasks = (req,res)=>{
     res.send("get all tasks");
 }
 
-const createTask = (req, res)=>{
+const createTask = async (req, res)=>{
     //res.send("create a task");
+    const task = await Task.create(req.body);
     res.json(req.body);
+    res.status(201).json({ task });
 }
 
 const getTask = (req, res)=>{
