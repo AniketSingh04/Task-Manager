f4bd236d181441951790f5138eb33bbb399d31b2
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-05-17 23:46:52 +0530

    code refactoring

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks.js");
+const connectDB = require('./controllers/db/connect.js')
+require("dotenv").config()
 
 //middleware
 app.use(express.json()) //for handling json requests
-
 
 //routes
 app.get("/hello", (req, res) =>{
@@ -14,23 +15,18 @@ app.get("/hello", (req, res) =>{
 
 app.use('/api/v1/tasks', tasks)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//App will run iff Connection to DB is successful
 const port = 3000;
-app.listen(3000, () => {
-    console.log(`Server is listening on port ${3000}`);
-})
+const start = async()=>{
+    try{
+        await connectDB(process.env.Mongo_URI)
+        app.listen(port, console.log(`Server is listening on port ${3000}`));
+    } 
+    catch(error){
+        console.log(error);
+    }
+}
+start();
+
+
+
