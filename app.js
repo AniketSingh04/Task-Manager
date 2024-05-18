dfbd8cd4081ba1e955ed78749a88862f5f2fc2bc
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-05-18 18:45:55 +0530

    Main Server

--- app.js
+++ app.js
@@ -6,13 +6,9 @@ require("dotenv").config()
 
 //middleware
 app.use(express.json()) //for handling json requests
+app.use(express.static("./public"));
 
 //routes
-app.get("/hello", (req, res) =>{
-    console.log("Hello");
-    res.send("Task Manager")
-})
-
 app.use('/api/v1/tasks', tasks)
 
 //App will run iff Connection to DB is successful
