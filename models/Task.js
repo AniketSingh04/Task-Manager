bf762295cb0a637775f040cc52af7069d3b5b1ee
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-05-18 18:45:10 +0530

    DB Schema and Model

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type : String,
+        required: [true,'must provide name'], //if the value to name is passed of any other type then error will occur
+        trim : true,
+        maxlength : [20, 'name cannot exceed 20 characters'],
+    },
+    completed: {
+        type : Boolean,
+        default: false,
+    }
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
