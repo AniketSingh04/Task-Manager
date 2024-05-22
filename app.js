9bdb26eadceb54b7d2e18cd9d002efa1481dd99a
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-05-22 15:50:13 +0530

    New middlewares added

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 const tasks = require("./routes/tasks.js");
 const connectDB = require('./controllers/db/connect.js')
 require("dotenv").config()
-
+const notFound = require("./middleware/not_found.js");
 //middleware
 app.use(express.json()) //for handling json requests
 app.use(express.static("./public"));
+app.use(notFound); //customized 404 middleware used
 
 //routes
 app.use('/api/v1/tasks', tasks)
